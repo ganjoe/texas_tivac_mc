e2771f1086f6f7d0a3ed9db045a0b435e38ce4b8
Daniel Meier <35268604+ganjoe@users.noreply.github.com>
2022-01-04 10:10:09 +0100

    fertig

--- propelli_v2_tiva/main.c
+++ propelli_v2_tiva/main.c
@@ -10,7 +10,7 @@
 
 #define RED_LED   GPIO_PIN_1
 
-
+size_t ramcounter;
 int main(void)
 {
 
@@ -23,17 +23,20 @@ int main(void)
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    
+
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))   { }
 
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED);
 
     UARTInit();
 
+    UARTprintf("System Clock@ %d Mhz\r",SysCtlClockGet()/1000000);
+    UARTprintf("Flash Size@ %d kbyte\r",SysCtlFlashSizeGet()/1000);
+    UARTprintf("SRAM Size@ %d kbyte\r",SysCtlSRAMSizeGet()/1000);
+
+    ramcounter = __TI_heap_total_available();
 
 
-
-    UARTprintf("Hello, world!\n");
 
     cmd_init_callbacks(&newcmd);
 
